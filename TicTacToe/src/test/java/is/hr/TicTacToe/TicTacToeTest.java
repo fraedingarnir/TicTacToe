f803d525271ae3ef17008a09df5dfb3cea75ee03
Hjalmar Diego Arnorsson <hjalmar15@i-1217c9a8.cs1004cloud.internal>
2016-10-25 13:47:53 +0000

    Refined test

--- TicTacToe/src/test/java/is/hr/TicTacToe/TicTacToeTest.java
+++ TicTacToe/src/test/java/is/hr/TicTacToe/TicTacToeTest.java
@@ -10,10 +10,20 @@ public class TicTacToeTest {
     org.junit.runner.JUnitCore.main("is.hr.TicTacToe.TicTacToeTest");
     }
 	
+	@Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Test
+    public void testPlayerTurn() {
+        TicTacToe.PlayerTurn();
+        assertEquals("Player X, your turn", systemOutRule.getLog());
+    }
 	
+	/*
 	@Test
 	public void testEmptyString() {
 		assertEquals("Player X, your turn", TicTacToe.PlayerTurn());
 	}
+	*/
 	
 }
