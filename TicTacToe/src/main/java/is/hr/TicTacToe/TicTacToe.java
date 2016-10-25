af95469a1dd4610674e073c7de27096b2f1cda30
Arnar Jóhannsson <arnarjoh15@i-1217c9a8.cs1004cloud.internal>
2016-10-25 11:35:02 +0000

    Java file committed

---
+++ TicTacToe/src/main/java/is/hr/TicTacToe/TicTacToe.java
@@ -0,0 +1,18 @@
+ckage TicTacToe;
+
+public class TicTacToe {
+	
+	public static final int ROWS = 3, COLS = 3;    
+	public static int[][] board = new int[ROWS][COLS];
+	
+	public void InitGame()
+	{
+    		for (int row = 0; row < ROWS; ++row) 
+		{
+    			for (int col = 0; col < COLS; ++col) 
+			{
+    				board[row][col] = 0;  
+    			}
+		}
+	}
+}
