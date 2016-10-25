404c3981d486683339a92f01ea62e8ec65cd5ecb
alexi15 <alexi15@ru.is>
2016-10-25 14:31:04 +0000

    print board function

--- TicTacToe/src/main/java/is/hr/TicTacToe/TicTacToe.java
+++ TicTacToe/src/main/java/is/hr/TicTacToe/TicTacToe.java
@@ -7,8 +7,13 @@ public class TicTacToe {
 	private static final char O = 'o';
 	private static int currentState; //
 	private static final int COL = 9;
-	private static char[] board = new char[COL];
+	private static char[] board;
 	private static boolean PlayerTurn;
+
+	public TicTacToe()
+	{
+			board = new char[COL];
+	}
 	
 	public void InitGame()
 	{
@@ -33,4 +38,26 @@ public class TicTacToe {
 			}
 		}
 	}
+
+	public void printBoard()
+	{
+		int line = 0;
+		for(int row = 0; row < 3; row++){
+			for(int col = line; col < line+3; col++)
+				if(col%3 != 0)
+					System.out.print(board[col] + " | ");
+			if(row != 2)
+				System.out.println("\n--|---|--");
+			line += 3;
+		}
+
+	}
+	/*
+	public static void main(String[] args) {
+			TicTacToe ttt = new TicTacToe();
+			ttt.InitGame();
+			ttt.printBoard();
+	}
+		*/
 }
+
