3dd632f28d4e892545725766b5d93ffc83349152
alexi15 <alexi15@ru.is>
2016-10-25 11:40:57 +0000

    change name of test file

---
+++ TicTacToe/src/test/java/is/hr/TicTacToe/TicTacToeTest.java
@@ -0,0 +1,14 @@
+package is.hr.TicTacToe;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import org.junit.Test;
+
+
+public class ChuckTest {
+
+    @Test
+    public void randomJokeContainsName() {
+        Chuck chuck = new Chuck();
+        assertThat(chuck.random(), containsString("Chuck Norris"));
