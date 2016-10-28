fa32b413157951478aa76803605d090d900ccf24
jonhei13 <jonhei13@ru.is>
2016-10-28 15:45:46 +0000

    PlayGame main class

---
+++ TicTacToe/src/main/java/is/hr/TicTacToe/PlayGame.java
@@ -0,0 +1,11 @@
+package is.hr.TicTacToe;
+
+public class Controller {
+	
+	public static void main(String[] args)
+	{
+		Player player1 = new Player('x');
+		Player player2 = new Player('o');
+		
+	}
+}
