139e5866041fec187ae55f3d6a43b2ecaba9d2ee
alexi15 <alexi15@ru.is>
2016-10-28 13:36:08 +0000

    Board constructor and addPlay function, and tests

--- TicTacToe/src/main/java/is/hr/TicTacToe/Board.java
+++ TicTacToe/src/main/java/is/hr/TicTacToe/Board.java
@@ -1,1 +1,30 @@
-Hello!
+package is.hr.TicTacToe;
+
+public class Board {
+	private static char[] board;
+
+	public Board()
+	{
+		board = new char[9];
+		init();
+	}
+
+	private void init()
+	{
+        	for (int col = 49; col < 49+ 9; ++col)
+        	{
+        		board[col-49] = (char) col;
+        	}
+	}
+
+	public boolean addPlay(int input, char symbol)
+	{
+		if(input < 1 || input > 9)
+			return false;
+		if(board[input-1] !=  (char)(input+48))
+			return false;
+
+		board[input-1] = symbol;
+		return true;
+	}
+}
