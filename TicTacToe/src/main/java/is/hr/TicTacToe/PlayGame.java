006e5f1776bd7aac6b0baec7612f1338745c1b97
alexi15 <alexi15@ru.is>
2016-10-28 16:31:16 +0000

    fix playgame

--- TicTacToe/src/main/java/is/hr/TicTacToe/PlayGame.java
+++ TicTacToe/src/main/java/is/hr/TicTacToe/PlayGame.java
@@ -4,8 +4,8 @@ public class PlayGame {
 	
 	public static void main(String[] args)
 	{
-		Player player1 = new Player('X');
-		Player player2 = new Player('O');
+		//Player player1 = new Player('X');
+		//Player player2 = new Player('O');
 		
 		Board board = new Board();
 		printBoard(board);
