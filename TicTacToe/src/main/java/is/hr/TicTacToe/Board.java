3118e0defae7634f394b749a0c49a233aa931774
Arnar Jóhannsson <arnarjoh15@i-1217c9a8.cs1004cloud.internal>
2016-10-28 12:32:16 +0000

    added Cons and init to Board and some global variables

--- TicTacToe/src/main/java/is/hr/TicTacToe/Board.java
+++ TicTacToe/src/main/java/is/hr/TicTacToe/Board.java
@@ -1,1 +1,23 @@
-Hello!
+package is.hr.TicTacToe
+public class Board {
+	
+	private static char[] board;
+	
+	private Player player1;
+	private Player player2;
+	
+	public Board(Player player1, Player player2)
+	{
+		this.player1 = player1;
+		this.player2 = player2;
+		board = new char[9];
+	}
+	
+	public void init()
+	{
+        for (int col = 49; col < 49+ 9; ++col)
+        {
+        	board[col-49] = (char) col;
+        }
+	}
+}
