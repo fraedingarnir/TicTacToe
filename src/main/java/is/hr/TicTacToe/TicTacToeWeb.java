99e333fb19fd748224c805cd24003bcdb43efd08
egill15 <egillb86@gmail.com>
2016-10-29 16:38:30 +0000

    web

--- src/main/java/is/hr/TicTacToe/TicTacToeWeb.java
+++ src/main/java/is/hr/TicTacToe/TicTacToeWeb.java
@@ -1,28 +1,30 @@
 package is.hr.TicTacToe;
+
 /*
 import spark.*;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
-
+*/
+/*
 public class TicTacToeWeb implements SparkApplication{
 	
 	    public static void main(String[] args) {
         staticFileLocation("/public");
-        SparkApplication chuckweb = new ChuckWeb();
+        SparkApplication TicWeb = new ChuckWeb();
 
         String port = System.getenv("PORT");
         if (port != null) {
             port(Integer.valueOf(port));
         }
 
-        TicTacToeWeb.init();
+        TicWeb.init();
     }
 	
 	@Override
     public void init() {
         final Board board = new Board();
 		
-        post("/play", (req, res) -> Board.addPlay(req.queryParams("id"), req.queryParams("symbol")));
+       // post("/play", (req, res) -> Board.addPlay(req.queryParams("id"), req.queryParams("symbol")));
 		
     }
 }
