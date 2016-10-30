96fca9268dd07205d8815729d27a73e9cb4d2241
egill15 <egillb86@gmail.com>
2016-10-30 04:07:44 +0000

    staticfile location

--- src/main/java/is/hr/TicTacToe/TicTacToeWeb.java
+++ src/main/java/is/hr/TicTacToe/TicTacToeWeb.java
@@ -8,7 +8,7 @@ import spark.servlet.SparkApplication;
 public class TicTacToeWeb implements SparkApplication{
 	
 	    public static void main(String[] args) {
-        staticFileLocation("/public");
+        staticFileLocation("/html");
         SparkApplication TicWeb = new TicTacToeWeb();
 
         String port = System.getenv("PORT");
