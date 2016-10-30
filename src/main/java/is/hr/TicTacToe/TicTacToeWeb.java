97eaa9d821aad4ab3be32a4d97caf704d6b03b5f
egill15 <egillb86@gmail.com>
2016-10-30 10:59:29 +0000

    removed debug from java web

--- src/main/java/is/hr/TicTacToe/TicTacToeWeb.java
+++ src/main/java/is/hr/TicTacToe/TicTacToeWeb.java
@@ -9,30 +9,25 @@ public class TicTacToeWeb implements SparkApplication{
 	
 	    public static void main(String[] args) {
 			
-		System.out.println("v1");
-		
         staticFileLocation("/public");
-		System.out.println("v2");
         SparkApplication TicWeb = new TicTacToeWeb();
-		System.out.println("v3");
         String port = System.getenv("PORT");
         if (port != null) {
             port(Integer.valueOf(port));
         }
-		System.out.println("port: "+ port);
-		System.out.println("virkar");
         TicWeb.init();
     }
 	
 	@Override
     public void init() {
-		System.out.println("virkar2");
         final Board board = new Board();
-		System.out.println("virkar3");
-		board.addPlay(1,'X');
 		System.out.println(board.getSquareElement(1));
+		post("/play", (req, res) -> {
+			//Board.addPlay(req.queryParams("id"), req.queryParams("symbol"));
 		
-		//post("/play", (req, res) -> Board.addPlay(req.queryParams("id"), req.queryParams("symbol")));
+		res.status(200);
+		return res;
+		});
 	   
     }
 }
